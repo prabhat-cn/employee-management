96a9e04cc98753b7eaf98ad6467626584d5e7362
Subhojit Mondal <subhojitm@capitalnumbers.com>
2021-05-28 11:12:54 +0530

    file added

---
+++ src/constant/index.js
@@ -0,0 +1,1 @@
+export const apiHost = 'https://officeemployee.herokuapp.com/api';
--- src/pages/DepartmentList.jsx
+++ src/pages/DepartmentList.jsx
@@ -1,54 +1,85 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import { Button, Modal } from 'antd';
+
+import API from '../api';
 
 const DepartmentList = () => {
 
-  const [department, setDepartment] = useState(null);
+  const [department, setDepartment] = useState([]);
+  const [singleDepartment, setSingleDepartment] = useState({name: '', _id: ''});
   const [loading, setLoading] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const getDepartment = async (deptName) => {
+  const getDepartment = async () => {
+    setLoading(true);
     try{
-      const deptData = await axios.get('https://officeemployee.herokuapp.com/api/department');
-      return deptData;
-
-    }catch(error){
-      throw error;
-    }
-  };
-
-  const getData = async () => {
-    try{
-      const data = await getDepartment();
-      setDepartment(data);
-      console.log('getData->', data);
+      const deptData = await API.get('/department');
+      setDepartment(deptData.data.data);
+      setLoading(false);
     }catch(error){
       console.log(error.message);
     }
+  };
+
+  const getDepartmentId = async (id) => {
+    try{
+      const deptData = await API.get(`/department/${id}`);
+      setSingleDepartment(deptData.data.data);
+      showModal();
+    }catch(error){
+      console.log(error.message);
+    }
+  };
+
+  const viewDetail = (id) => {
+    getDepartmentId(id);
   }
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   useEffect(() => {
-    getData()
-  }, [])
+    getDepartment();
+  }, []);
   
   return (
-    <table class="table">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-        </tr>
-      </thead>
-      <tbody>
-        {department !== null ? (
-          <tr key={department._id}>
-          <th scope="row">{department.data.data[0]._id}</th>
-          <td>{department.data.data.name}</td>
-        </tr>
-        ) : null}
-        
-      </tbody>
-  </table>
-
+    <>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <h2>{singleDepartment.name}</h2>
+      </Modal>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {department.map((m) => (
+            <tr key={m._id}>
+              <td scope="row">{m._id}</td>
+              <td>{m.name}</td>
+              <td>
+                <Button type="primary" onClick={() => viewDetail(m._id)}>
+                  View Details
+                </Button>
+              </td>
+            </tr>
+          ))}
+          
+        </tbody>
+    </table>
+  </>
   )
 }
 
